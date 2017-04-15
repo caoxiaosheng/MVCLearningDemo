7d3940151cd373474fdd2345d9c88d858bcdc692
caoxiaosheng <836900367@qq.com>
2017-04-15 22:49:51 +0800

    Test29 自定义错误界面

---
+++ MVCLearningDemo/Controllers/ErrorController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLearningDemo.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            Exception exception=new Exception("非法控制器和（或）行为");
+            // ReSharper disable once Mvc.ControllerNotResolved
+            // ReSharper disable once Mvc.ActionNotResolved
+            HandleErrorInfo handleErrorInfo=new HandleErrorInfo(exception,"UnKnown", "UnKnown");
+            return View("Error",handleErrorInfo);
+        }
+    }
+}
